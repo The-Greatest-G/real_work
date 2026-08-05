461b60c863cbde3835fc856a8c1349808a0c7462
gift <mlachagift@gmail.com>
2026-08-06 01:02:03 +0300

    fix: finishing

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+﻿import React from 'react'
+import Topbar from './components/Topbar'
+import { Outlet } from '@tanstack/react-router'
+
+export default function App(){
+  return (
+    <div className='min-h-screen'>
+      <Topbar />
+      <main className='max-w-6xl mx-auto p-6'>
+        <Outlet />
+      </main>
+    </div>
+  )
+}
